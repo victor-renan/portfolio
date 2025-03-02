d2aff466cab191560e80820e742e8814245a952c
Renan <alvesrenanpro@gmail.com>
2025-03-02 06:28:17 -0300

    feat: complete portfolio code

---
+++ src/components/LinkContact.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+interface LinkContactProps {
+    url: string
+    title: string
+    icon: string
+}
+
+const props = defineProps<LinkContactProps>()
+
+</script>
+
+<template>
+    <a class="flex items-center justify-center gap-1 bg-stone-700 dark:bg-stone-500 p-2 rounded-lg text-amber-200 dark:text-stone-50 hover:bg-stone-600 dark:hover:bg-stone-700 transition"
+        target="_blank" :href="props.url">
+        <i :class="props.icon" class="text-xl"></i>
+        <span class="items-center gap-1 text-sm text-nowrap">
+            {{ props.title }}
+            <i class="bx bx-link-external"></i>
+        </span>
+    </a>
+</template>
---
+++ src/components/LinkProject.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+interface LinkProjectProps {
+    title: string
+    description: string
+    url: string
+    imageUrl: string
+    imageAlt?: string
+}
+
+const props = withDefaults(defineProps<LinkProjectProps>(), {
+    imageAlt: 'Foto do projeto'
+})
+
+</script>
+
+<template>
+    <a class="lg:max-w-60 transition hover:bg-stone-900 dark:hover:bg-stone-300 p-2 rounded-xl" target="_blank" :href="props.url">
+        <img class="rounded-lg w-full object-cover" :src="props.imageUrl" :alt="props.imageAlt">
+        <div class="mt-2">
+            <h1 class="text-xl font-bold mb-1 flex items-center gap-1">
+                {{ props.title }}
+                <i class="bx bx-link-external text-1xl"></i>
+            </h1>
+            <p class="text-sm">{{ props.description }}</p>
+        </div>
+    </a>
+</template>
