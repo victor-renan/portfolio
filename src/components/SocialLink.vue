c56534d68ad2068cee7343cbf85b24419ab8028b
Renan Alves <alvesrenanpro@gmail.com>
2026-04-18 12:15:23 -0300

    fix: layout changes init

---
+++ src/components/SocialLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+
+defineProps<{
+  icon: Component
+  text: string
+  link: string
+}>()
+</script>
+
+<template>
+  <a target="_blank" class="flex justify-center items-center px-4 py-2 gap-2 rounded-lg bg-bg-surface-3
+    hover:bg-bg-surface-4" :href="link" rel="noopener noreferrer">
+    <component :is="icon" class="size-6"></component>
+    <span class="font-medium">{{ text }}</span>
+  </a>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,20 @@ import './assets/css/styles.css'
 import 'boxicons/css/boxicons.min.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import pt from './i18n/locales/pt.json'
+
+const i18n = createI18n({
+  locale: 'pt',
+  fallbackLocale: 'pt',
+  messages: {
+    pt,
+  },
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+app.mount('#app')
